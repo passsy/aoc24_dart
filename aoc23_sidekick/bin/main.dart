cd994479e02600844c07e0e96823ca47e968e2c7
Pascal Welsch <pascal@welsch.dev>
2023-12-01 20:02:05 +0100

    Setup aoc23

---
+++ aoc23_sidekick/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:aoc23_sidekick/aoc23_sidekick.dart';
+
+Future<void> main(List<String> arguments) async {
+  await runAoc23(arguments);
+}
---
+++ aoc23_sidekick/lib/aoc23_sidekick.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:aoc23_sidekick/src/commands/clean_command.dart';
+import 'package:sidekick_core/sidekick_core.dart';
+import 'package:aoc23_sidekick/src/commands/new_day_command.dart';
+
+Future<void> runAoc23(List<String> args) async {
+  final runner = initializeSidekick(
+    dartSdkPath: systemDartSdkPath(),
+  );
+
+  runner
+    ..addCommand(DartCommand())
+    ..addCommand(DepsCommand())
+    ..addCommand(CleanCommand())
+    ..addCommand(DartAnalyzeCommand())
+    ..addCommand(FormatCommand())
+    ..addCommand(SidekickCommand())
+    ..addCommand(NewDayCommand());
+
+  try {
+    return await runner.run(args);
+  } on UsageException catch (e) {
+    print(e);
+    exit(64); // usage error
+  }
+}
