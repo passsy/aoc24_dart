3475370e51385c7c0b6f40d6228ac7b8cafab02a
Pascal Welsch <pascal@welsch.dev>
2023-12-01 20:21:38 +0100

    Not golfing this year

--- test/day01_test.dart
+++ test/day01_test.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 import '../bin/day01_part1.dart' as day01_part1;
-import '../bin/day01_part1.min.dart' as day01_part1_min;
 import '../bin/day01_part2.dart' as day01_part2;
-import '../bin/day01_part2.min.dart' as day01_part2_min;
 import 'main_tester.dart';
 
 void main() {
@@ -12,14 +10,6 @@ void main() {
     test('sample part 1', () {
       final output = testMain(
         day01_part1.main,
-        input: File('data/day01_sample.txt').readAsStringSync(),
-      );
-      expect(output, '142');
-    });
-
-    test('golf part 1', () {
-      final output = testMain(
-        day01_part1_min.main,
         input: File('data/day01_sample.txt').readAsStringSync(),
       );
       expect(output, '142');
@@ -37,14 +27,6 @@ void main() {
     test('sample part 2', () {
       final output = testMain(
         day01_part2.main,
-        input: File('data/day01_sample2.txt').readAsStringSync(),
-      );
-      expect(output, '281');
-    });
-
-    test('golf part 2', () {
-      final output = testMain(
-        day01_part2_min.main,
         input: File('data/day01_sample2.txt').readAsStringSync(),
       );
       expect(output, '281');
