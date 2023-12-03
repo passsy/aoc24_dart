6e11ae075b96691e8c45e08e3b6a8613311140fc
Pascal Welsch <pascal@welsch.dev>
2023-12-03 15:50:17 +0100

    Solve Day 3 Part 1

---
+++ bin/day03_part1.dart
@@ -0,0 +1,84 @@
+void main(List<String> args) {
+  final map = parseMap(args);
+  final numbersOnMap = map.where((it) => it.number != null).toList();
+  final symbols = map.where((it) => it.symbol != null).toList();
+
+  final Set<PointOnMap> allTouchingNumbersSet = {};
+  for (final symbol in symbols) {
+    final adjacent = map
+        .where((it) {
+          return (it.x - symbol.x).abs() <= 1.9 &&
+              (it.y - symbol.y).abs() <= 1.9;
+        })
+        .where((it) => it.number != null)
+        .toList();
+    allTouchingNumbersSet.addAll(adjacent);
+  }
+
+  final allTouchingNumbersList = allTouchingNumbersSet.toList();
+  int sum = 0;
+
+  while (allTouchingNumbersList.isNotEmpty) {
+    // find beginning on the left
+    PointOnMap point = allTouchingNumbersList.last;
+    PointOnMap left = point;
+    PointOnMap? found;
+    do {
+      found = numbersOnMap
+          .firstWhereOrNull((it) => it.x == left.x - 1 && it.y == left.y);
+      if (found != null) {
+        left = found;
+      }
+    } while (found != null);
+
+    // iterate right to read the whole number
+    final start = left;
+    allTouchingNumbersList.remove(start);
+    String number = start.number.toString();
+    PointOnMap right = start;
+    do {
+      found = numbersOnMap
+          .firstWhereOrNull((it) => it.x == right.x + 1 && it.y == right.y);
+      if (found != null) {
+        right = found;
+        number += right.number.toString();
+        allTouchingNumbersList.remove(found);
+      }
+    } while (found != null);
+
+    sum += int.parse(number);
+  }
+  print(sum);
+}
+
+typedef PointOnMap = ({int? number, String? symbol, int x, int y});
+
+List<PointOnMap> parseMap(List<String> args) {
+  final List<({int x, int y, String? symbol, int? number})> map = [];
+  for (int y = 0; y < args.length; y++) {
+    final line = args[y];
+    final chars = line.split('');
+    for (int x = 0; x < chars.length; x++) {
+      final char = chars[x];
+      if (char == '.') {
+        continue;
+      }
+      if (int.tryParse(char) != null) {
+        map.add((x: x, y: y, symbol: null, number: int.parse(char)));
+      } else {
+        map.add((x: x, y: y, symbol: char, number: null));
+      }
+    }
+  }
+  return map;
+}
+
+extension FirstWhereOrNull<T> on List<T> {
+  T? firstWhereOrNull(bool Function(T) test) {
+    try {
+      return firstWhere(test);
+    } catch (e) {
+      return null;
+    }
+  }
+}
---
+++ test/day03_test.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import '../bin/day03_part1.dart' as day03_part1;
+import '../bin/day03_part2.dart' as day03_part2;
+import 'main_tester.dart';
+
+void main() {
+  group('day 03', () {
+    test('sample part 1', () {
+      final output = testMain(
+        day03_part1.main,
+        input: File('data/day03_sample.txt').readAsStringSync(),
+      );
+      expect(output, '4361');
+    });
+    test('solve part 1', () {
+      final output = testMain(
+        day03_part1.main,
+        input: File('data/day03_input.txt').readAsStringSync(),
+      );
+      expect(output, isNot('0'));
+      print(output);
+    });
+    test('sample part 2', () {
+      final output = testMain(
+        day03_part2.main,
+        input: File('data/day03_sample.txt').readAsStringSync(),
+      );
+      expect(output, '100');
+    });
+    test('solve part 2', () {
+      final output = testMain(
+        day03_part2.main,
+        input: File('data/day03_input.txt').readAsStringSync(),
+      );
+      expect(output, isNot('0'));
+      print(output);
+    });
+  });
+}
