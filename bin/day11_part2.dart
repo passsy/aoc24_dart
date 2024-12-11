35341ebd0762366fd76d6fb3eedfe2cc5cc73359
Pascal Welsch <pascal@phntm.xyz>
2024-12-11 07:37:28 +0100

    Simplify Part 2

--- bin/day11_part2.dart
+++ bin/day11_part2.dart
@@ -1,62 +1,50 @@
 import 'package:dartx/dartx.dart';
 
+extension type Stone(int number) {}
+
 void solveDay11(String input) {
   // https://adventofcode.com/2024/day/11
   // Solve Part 2 here
 
-  final List<int> stones = input.split(' ').map(int.parse).toList().toList();
+  final Map<Stone, int> stones = input
+      .split(' ')
+      .map(int.parse)
+      .groupBy((stone) => stone)
+      .map((key, value) => MapEntry(Stone(key), value.length));
+
   print(blink(75, stones));
 }
 
-int blink(int n, List<int> input) {
-  // save stone and count in map. Order does not matter
-  Map<int, int> stones = {};
-  for (final stone in input) {
-    stones.putIfAbsent(stone, () => 0);
-    final count = stones[stone]!;
-    stones[stone] = count + 1;
+int blink(int n, Map<Stone, int> stones) {
+  if (n == 0) {
+    return stones.stoneCount;
   }
 
-  for (int blink = 1; blink <= n; blink++) {
-    final Map<int, int> newStones = {};
-    for (final s in stones.entries) {
-      final stone = s.key;
-      final amount = s.value;
-      final split = splitStone(stone);
-      for (final s in split) {
-        newStones.putIfAbsent(s, () => 0);
-        final count = newStones[s]!;
-        newStones[s] = count + amount;
-      }
+  final newStones = <Stone, int>{};
+  for (final MapEntry(key: stone, value: amount) in stones.entries) {
+    for (final split in splitStone(stone)) {
+      newStones[split] = (newStones[split] ?? 0) + amount;
     }
-    stones = newStones;
-    final count = stones.mapEntries((e) => e.value).sum();
-    print('After ${blink} blink, stones: ${count}');
   }
-  final count = stones.mapEntries((e) => e.value).sum();
-  return count;
+
+  print('After ${75 - n + 1} blink, stones: ${newStones.stoneCount}');
+  return blink(n - 1, newStones);
 }
 
-// memoization
-final known = <int, List<int>>{};
+List<Stone> splitStone(Stone stone) {
+  if (stone.number == 0) {
+    return [Stone(1)];
+  }
+  final stoneString = stone.number.toString();
+  if (stoneString.length % 2 == 0) {
+    final half = stoneString.length ~/ 2;
+    final firstHalf = int.parse(stoneString.substring(0, half));
+    final secondHalf = int.parse(stoneString.substring(half));
+    return [Stone(firstHalf), Stone(secondHalf)];
+  }
+  return [Stone(stone.number * 2024)];
+}
 
-List<int> splitStone(int stone) {
-  final isKnown = known[stone];
-  if (isKnown != null) {
-    return isKnown;
-  }
-  final List<int> output = [];
-  if (stone == 0) {
-    output.add(1);
-  } else if (stone.toString().length % 2 == 0) {
-    final half = stone.toString().length ~/ 2;
-    final firstHalf = int.parse(stone.toString().substring(0, half));
-    final secondHalf = int.parse(stone.toString().substring(half));
-    output.add(firstHalf);
-    output.add(secondHalf);
-  } else {
-    output.add(stone * 2024);
-  }
-  known[stone] = output;
-  return output;
+extension on Map<Stone, int> {
+  int get stoneCount => mapEntries((e) => e.value).sum();
 }
